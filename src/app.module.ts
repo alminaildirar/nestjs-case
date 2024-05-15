73a4fd5feb2971c81779e6cabe20e56a97c6d77b
almina <alminaildirar@gmail.com>
2024-05-16 01:04:21 +0300

    mongoose config service is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpClientModule } from './common/http-client/http-client.module';
 import { ConfigurationModule } from './config/configuration.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose.config';
+import { ConfigurationService } from './config/configuration.service';
 
 @Module({
-  imports: [HttpClientModule, ConfigurationModule],
+  imports: [
+    HttpClientModule,
+    ConfigurationModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigurationModule],
+      useClass: MongooseConfigService,
+      inject: [ConfigurationService],
+    }),
+  ],
   controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { ConfigurationService } from './config/configuration.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigurationService);
+  
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
-  await app.listen(3000);
+  
+  const port = configService.port || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

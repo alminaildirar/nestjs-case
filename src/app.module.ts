6f5ace12e58da38f111383a5313e640fa865bb50
almina <alminaildirar@gmail.com>
2024-05-15 22:32:06 +0300

    config service is added for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpClientModule } from './common/http-client/http-client.module';
+import { ConfigurationModule } from './config/configuration.module';
 
 @Module({
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, ConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
